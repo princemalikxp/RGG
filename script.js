1e4a2f69a5bc667801d8e2a5766dd859f21eb849
Malik <103148219+princemalikxp@users.noreply.github.com>
2023-12-12 03:07:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,38 +3,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const backgroundDiv = document.getElementById('backgroundDiv');
 
     playButton.addEventListener('click', function () {
-        fetchRandomAnimeGif();
+        setLocalBackgroundGif('path/to/your/local/anime.gif');
     });
 
-    async function fetchRandomAnimeGif() {
-        const apiKey = 'qG0FZ6oNPwcvNswx35dzHHuHfsY9qDSy'; // Replace with your Giphy API key
-        const limit = 1; // Number of gifs to retrieve
-
-        const apiUrl = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=anime&rating=g&limit=${limit}`;
-
-        try {
-            const response = await fetch(apiUrl);
-
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-
-            const data = await response.json();
-
-            if (data.data) {
-                const gifUrl = data.data.image_original_url;
-                applyBackgroundGif(gifUrl);
-            } else {
-                console.error('No gifs found in the response data:', data);
-            }
-        } catch (error) {
-            console.error('Error fetching or parsing data:', error);
-        }
-    }
-
-    function applyBackgroundGif(gifUrl) {
+    function setLocalBackgroundGif(gifPath) {
         backgroundDiv.style.cssText = `
-            background: url(${gifUrl}) no-repeat center center fixed;
+            background: url(${gifPath}) no-repeat center center fixed;
             background-size: cover;
             animation: fadeIn 2s ease-in-out;
         `;
