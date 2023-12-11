b457af690ee1453e055c4c4a80e6b144d5633eeb
Malik <103148219+princemalikxp@users.noreply.github.com>
2023-12-12 00:27:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const playButton = document.getElementById('playButton');
-    const gifContainer = document.getElementById('gifContainer');
+async function fetchRandomAnimeGif() {
+    const apiKey = 'YOUR_TENOR_API_KEY'; // Replace with your Tenor API key
+    const limit = 1; // Number of gifs to retrieve
 
-    playButton.addEventListener('click', function () {
-        fetchRandomAnimeGif();
-    });
+    const apiUrl = `https://api.tenor.com/v1/random?q=anime&key=${apiKey}&limit=${limit}`;
 
-    async function fetchRandomAnimeGif() {
-        const apiKey = 'AIzaSyAtRbVWltUMe3MHMUqiaEfZm1844lZ9Ymc'; // Replace with your Tenor API key
-        const limit = 1; // Number of gifs to retrieve
+    try {
+        const response = await fetch(apiUrl, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        const data = await response.json();
 
-        const apiUrl = `https://api.tenor.com/v1/random?q=anime&key=${apiKey}&limit=${limit}`;
-
-        try {
-            const response = await fetch(apiUrl);
-            const data = await response.json();
-
-            if (data.results && data.results.length > 0) {
-                const gifUrl = data.results[0].media[0].gif.url;
-                displayGif(gifUrl);
-            } else {
-                console.error('No gifs found.');
-            }
-        } catch (error) {
-            console.error('Error fetching data:', error);
+        if (data.results && data.results.length > 0) {
+            const gifUrl = data.results[0].media[0].gif.url;
+            displayGif(gifUrl);
+        } else {
+            console.error('No gifs found.');
         }
+    } catch (error) {
+        console.error('Error fetching data:', error);
     }
-
-    function displayGif(gifUrl) {
-        gifContainer.innerHTML = `<img src="${gifUrl}" alt="Random Anime Gif" style="max-width: 100%;">`;
-    }
-});
+}
